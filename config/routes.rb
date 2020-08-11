634d7e052ce93a940ea8070ffa144d151b4aff80
Jeroen Zwart <jeroen.zwart@live.com>
2020-08-11 16:50:48 +0200

    finished basic app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # read all
+  get '/tasks', to: 'tasks#index'
+
+  # create a task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # update a task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
